ad46078ede5d99e78b72259ee07005140333e798
Michael Liao <askxuefeng@gmail.com>
2014-12-28 12:37:30 +0800

    add Token enum

---
+++ src/main/java/com/itranswarp/jsonstream/Token.java
@@ -0,0 +1,17 @@
+package com.itranswarp.jsonstream;
+
+public enum Token {
+
+    END_DOCUMENT,
+    START_OBJECT,
+    END_OBJECT,
+    START_ARRAY,
+    END_ARRAY,
+    COLON_SEPERATOR,
+    COMMA_SEPERATOR,
+    STRING,
+    BOOLEAN,
+    NUMBER,
+    NULL
+
+}

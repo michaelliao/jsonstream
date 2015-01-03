a1f51cfe8a5685ab42b3a67a60ef9a526357e0cd
Michael Liao <askxuefeng@gmail.com>
2015-01-03 21:40:51 +0800

    update pom.xml

--- src/main/java/com/itranswarp/jsonstream/ObjectHook.java
+++ src/main/java/com/itranswarp/jsonstream/ObjectHook.java
@@ -2,8 +2,22 @@ package com.itranswarp.jsonstream;
 
 import java.util.Map;
 
+/**
+ * Interface used to convert a JSON object (Map<String, Object>) to Java object.
+ * 
+ * @author Michael Liao
+ */
 public interface ObjectHook {
 
+    /**
+     * Convert Map<String, Object> to Java bean.
+     * 
+     * @param path The path of the JSON.
+     * @param map Key-value pairs.
+     * @param clazz The expected Java class.
+     * @param typeAdapters Registered type-adapters to convert String to any target type.
+     * @return
+     */
     Object toObject(String path, Map<String, Object> map, Class<?> clazz, TypeAdapters typeAdapters);
 
 }

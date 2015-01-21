a9f9929a4180df0d930f0197de26dc787687585f
Michael Liao <askxuefeng@gmail.com>
2015-01-21 16:57:58 +0800

    add createWriter by outputstream

--- src/main/java/com/itranswarp/jsonstream/JsonBuilder.java
+++ src/main/java/com/itranswarp/jsonstream/JsonBuilder.java
@@ -2,6 +2,8 @@ package com.itranswarp.jsonstream;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
@@ -64,6 +66,17 @@ public class JsonBuilder {
     }
 
     public JsonWriter createWriter(Writer writer) {
+        return new JsonWriter(writer, typeAdapters);
+    }
+
+    public JsonWriter createWriter(OutputStream output) {
+        Writer writer = null;
+        try {
+            writer = new OutputStreamWriter(output, "UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
         return new JsonWriter(writer, typeAdapters);
     }
 }
